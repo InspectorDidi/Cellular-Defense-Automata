5b19cac2630abc376cba0b644d562b2b6c4a69ec
Tara <77855700+TheMemeticist@users.noreply.github.com>
2021-02-24 21:32:15 -0700

    Update CellularDefenseAutomata.py

--- CellularDefenseAutomata.py
+++ CellularDefenseAutomata.py
@@ -1,4 +1,8 @@
-
-
-if __name__ == '__main__':
-	main()
+import numpy as B
+class C:
+	def __init__(B,size=100):B.Board=A((size,size))
+	def Play(A,days=365*3):A.Days=days
+	def Render(A,InConsole=True):0
+class A:
+	def __init__(A,size):A.Size=size;A.Build()
+	def Build(A):0
