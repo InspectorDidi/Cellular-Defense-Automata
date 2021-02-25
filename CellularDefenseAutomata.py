e93ce7628f821fdaeb80107f853b082227e82fff
Tara <77855700+TheMemeticist@users.noreply.github.com>
2021-02-24 22:19:12 -0700

    Update CellularDefenseAutomata.py

--- CellularDefenseAutomata.py
+++ CellularDefenseAutomata.py
@@ -1,8 +1,1 @@
-import numpy as B
-class C:
-	def __init__(B,size=100):B.Board=A((size,size))
-	def Play(A,days=365*3):A.Days=days
-	def Render(A,InConsole=True):0
-class A:
-	def __init__(A,size):A.Size=size;A.Build()
-	def Build(A):0
+exec("""\nexec(\"\"\"\\n\\nimport numpy as B\\nclass C:\\n	def __init__(B,size=100):B.Board=A((size,size))\\n	def Play(A,days=365*3):A.Days=days\\n	def Render(A,InConsole=True):0\\nclass A:\\n	def __init__(A,size):A.Size=size;A.Build()\\n	def Build(A):0\\n\"\"\")\n""")
